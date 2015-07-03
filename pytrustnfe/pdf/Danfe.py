c1ea862475e555a970ec4b6ab43c23620e6f5a3d
Danimar Ribeiro <danimaribeiro@gmail.com>
2015-07-03 16:03:57 -0300

    Parte de código do danfe, correção na geração do xml para permitir unicode

--- pytrustnfe/pdf/Danfe.py
+++ pytrustnfe/pdf/Danfe.py
@@ -10,6 +10,8 @@ from reportlab.pdfgen.canvas import Canvas
 from reportlab.lib.pagesizes import A4
 from reportlab.platypus.doctemplate import SimpleDocTemplate
 from reportlab.lib import colors
+from reportlab.platypus.paragraph import Paragraph
+from reportlab.lib.styles import ParagraphStyle
 
 inch = 28.34
 
@@ -23,14 +25,7 @@ class Danfe(object):
             'ObjetoNFe deve ser do tipo DynamicXml'
         self.objeto = objetoNFe
 
-    def gerar(self):
-        doc = SimpleDocTemplate(
-            '/home/danimar/projetos/pdfs/danfe.pdf',
-            pagesize=A4, leftMargin=0.5 * inch, rightMargin=0.5 * inch,
-            topMargin=0.5 * inch, bottomMargin=0.5 * inch)
-
-        elementos = []
-
+    def _header(self):
         data = [
             ['Recebemos de verdesaine industria e comércio os produtos constantes na nota fiscal abaixo ', '',
              'NF-e\nNº 000.000.001\nSérie 001'],
@@ -41,13 +36,45 @@ class Danfe(object):
 
         estilo = [('SPAN', (0, 0), (1, 0)),
                   ('SPAN', (2, 0), (2, 1)),
-                  ('FONTSIZE', (0, 0), (1, 1), 8.0),
+                  ('FONTSIZE', (0, 0), (1, 1), 7.0),
                   ('VALIGN', (0, 0), (1, 1), 'TOP'),
                   ('ALIGN', (2, 0), (2, 1), 'CENTER'),
-                  ('GRID', (0, 0), (3, 1), 1, colors.black)]
+                  ('TOPPADING', (0, 0), (1, 1), 6),
+                  ('GRID', (0, 0), (3, 1), 0.5, colors.black)]
         colunas = [4 * inch, 12 * inch, 4 * inch]
+        linhas = [20,30]
+        table = Table(data, style=estilo, colWidths=colunas, rowHeights=linhas)
+        return table
+    
+    def _field(self, label, value):
+        estilo = ParagraphStyle('default')
+        return Paragraph('<font size="8">' + label + '</font>' + '<br />' + value, estilo)
+    
+    def _segundo_cabecalho(self):
+        data = [
+                [self._field('Natureza da operação', 'Venda de produção do estabelecimento'), '',
+                 self._field('Protocolo de autorização de uso', '12345678956665487')],
+                [self._field('Inscrição estadual', '156466487897'), 
+                 self._field('Inscrição estadual substituto tributário', '1456465456'), 
+                 self._field('CNPJ', '87.224.633/0001-61'), ]
+                ]
+        
+        estilo = [('SPAN', (0, 0), (1, 0)),                  
+                  ('FONTSIZE', (0, 0), (1, 1), 7.0),
+                  ('GRID', (0, 0), (2, 1), 0.5, colors.black)]
+        colunas = [6 * inch, 7 * inch, 7 * inch]        
         table = Table(data, style=estilo, colWidths=colunas)
+        return table
+    
+    def gerar(self):
+        doc = SimpleDocTemplate(
+            '/home/danimar/projetos/pdfs/danfe.pdf',
+            pagesize=A4, leftMargin=0.5 * inch, rightMargin=0.5 * inch,
+            topMargin=0.5 * inch, bottomMargin=0.5 * inch)
 
-        elementos.append(table)
+        elementos = []
+        
+        elementos.append(self._header())
+        elementos.append(self._segundo_cabecalho())
 
         doc.build(elementos)
--- pytrustnfe/xml/DynamicXml.py
+++ pytrustnfe/xml/DynamicXml.py
@@ -1,4 +1,4 @@
-#coding=utf-8
+# coding=utf-8
 '''
 Created on Jun 17, 2015
 
@@ -7,41 +7,46 @@ Created on Jun 17, 2015
 
 import xml.etree.ElementTree as ET
 from lxml.etree import Element, tostring
+from __builtin__ import str
 
-class DynamicXml(object):    
+
+class DynamicXml(object):
+
     def __getattr__(self, name):
-        try:          
-            return object.__getattribute__(self,name)
-        except:           
+        try:
+            return object.__getattribute__(self, name)
+        except:
             self.__setattr__(name, None)
-            return object.__getattribute__(self,name)
-            
+            return object.__getattribute__(self, name)
+
     def __setattr__(self, obj, val):
-        if(obj=="value" or obj=="atributos" or obj=="_indice"):
-            object.__setattr__(self,obj, val)
+        if(obj == "value" or obj == "atributos" or obj == "_indice"):
+            object.__setattr__(self, obj, val)
         else:
             self._indice = self._indice + 1
-            object.__setattr__(self,obj, DynamicXml(val, self._indice))
+            object.__setattr__(self, obj, DynamicXml(val, self._indice))
 
     def __init__(self, value, indice=0):
-        self.value = value
-        self.atributos={}
+        self.value = unicode(value, 'utf-8') if isinstance(value,
+                                                           str) else value
+        self.atributos = {}
         self._indice = indice
-               
+
     def __str__(self):
-        return str(self.value)
-    def __call__(self, *args, **kw):        
-        if(len(kw)>0):
-            self.atributos=kw
-        if(len(args)>0):
-            self.value= args[0]
+        return unicode(self.value)
+
+    def __call__(self, *args, **kw):
+        if(len(kw) > 0):
+            self.atributos = kw
+        if(len(args) > 0):
+            self.value = args[0]
         else:
             return self.value
 
     def __getitem__(self, i):
         if not isinstance(self.value, list):
             self.value = []
-        if(i+1 > len(self.value)):
+        if(i + 1 > len(self.value)):
             self.value.append(DynamicXml(None))
         return self.value[i]
 
@@ -49,24 +54,23 @@ class DynamicXml(object):
         root = Element(self.value)
         self._gerar_xml(root, self)
         return tostring(root, pretty_print=pretty_print)
-        
+
     def _gerar_xml(self, xml, objeto):
         items = sorted(
-            objeto.__dict__.items(), 
+            objeto.__dict__.items(),
             key=lambda x: x[1]._indice if isinstance(x[1], DynamicXml) else 0
         )
         for attr, value in items:
-            if(attr!="value" and attr!="atributos" and attr!="_indice"):
-                if isinstance(value(), list):                    
+            if(attr != "value" and attr != "atributos" and attr != "_indice"):
+                if isinstance(value(), list):
                     for item in value():
-                        sub = ET.SubElement(xml, attr) 
+                        sub = ET.SubElement(xml, attr)
                         self._gerar_xml(sub, item)
                 else:
-                    sub = ET.SubElement(xml, attr)                
-                    if(str(value)!="None"):
-                        sub.text = str(value)
+                    sub = ET.SubElement(xml, attr)
+                    if(unicode(value) != u"None"):
+                        sub.text = unicode(value)
                     self._gerar_xml(sub, value)
-            elif(attr=="atributos"):
-                for atr, val in value.items():                
+            elif(attr == "atributos"):
+                for atr, val in value.items():
                     xml.set(atr.replace("__", ":"), str(val))
-
