d433a10841ef72a16f63a5cc1d47901761b18728
Danimar <danimaribeiro@gmail.com>
2016-09-16 22:46:20 -0300

    Adicionando dependencias ao signxml

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
 # coding=utf-8
+import pypandoc
 from setuptools import setup, find_packages
 
+long_description = pypandoc.convert('README.md', 'rst')
+
 setup(
-    name="PyNfeTrust",
-    version="0.1",
+    name="PyTrustNFe",
+    version="0.1.2",
     author="Danimar Ribeiro",
     author_email='danimaribeiro@gmail.com',
     keywords=['nfe', 'mdf-e'],
     classifiers=[
-        'Development Status :: 1 - alpha',
+        'Development Status :: 3 - Alpha',
         'Environment :: Plugins',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU Lesser General Public License v2 or \
-        later (LGPLv2+)',
+later (LGPLv2+)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules',
@@ -20,13 +23,13 @@ setup(
     packages=find_packages(exclude=['*test*']),
     package_data={'pytrustnfe': ['nfe/templates/*xml',
                                  'nfse/paulistana/templates/*xml']},
-    url='https://github.com/danimaribeiro/PyNfeTrust',
+    url='https://github.com/danimaribeiro/PyTrustNFe',
     license='LGPL-v2.1+',
-    description='PyNfeTrust é uma biblioteca para envio de NF-e',
-    long_description='PyNfeTrust',
+    description='PyTrustNFe é uma biblioteca para envio de NF-e',
+    long_description=long_description,
     install_requires=[
         'Jinja2 >= 2.8',
-        'signxml >= 1.0.0',
+        'signxml >= 2.0.0',
     ],
     test_suite='nose.collector',
     tests_require=[
