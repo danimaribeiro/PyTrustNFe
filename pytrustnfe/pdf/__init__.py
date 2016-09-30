5bb541ef8f375ad7c9f967d47209e766ec7697ab
martini97 <alessandrofmartini@gmail.com>
2016-09-30 19:31:10 -0300

    Fixed the import

--- pytrustnfe/pdf/__init__.py
+++ pytrustnfe/pdf/__init__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from super_table import PdfTable, PdfCell
-from .xml import sanitize_response
+from pytrustnfe import xml
 
 inch = 28.34
 
