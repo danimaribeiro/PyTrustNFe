9300ebd8246321206c6769a254e2cceabcbad6f2
martini97 <alessandrofmartini@gmail.com>
2016-10-06 15:08:45 -0300

    Correção no build danfe

--- pytrustnfe/pdf/__init__.py
+++ pytrustnfe/pdf/__init__.py
@@ -2,7 +2,7 @@
 # © 2016 Danimar Ribeiro, Trustcode
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from super_table import PdfTable, PdfCell
+# from super_table import PdfTable, PdfCell
 from pytrustnfe import xml
 
 inch = 28.34
@@ -13,17 +13,21 @@ class Danfe(object):
     objeto = None
 
     def __init__(self, objetoNFe):
-        self.objeto = xml.sanitize_response(objetoNFe)[1]
-        self.NFe = self.objeto.getchildren()[2]
-        self.infNFe = self.NFe.getchildren()[0]
-        self.ide = self.infNFe.getchildren()[0]
-        self.emitente = self.infNFe.getchildren()[1]
-        self.destinatario = self.infNFe.getchildren()[2]
+        if objetoNFe:
+            self.objeto = xml.sanitize_response(objetoNFe)[1]
+            self.NFe = self.objeto.getchildren()[2]
+            self.infNFe = self.NFe.getchildren()[0]
+            self.ide = self.infNFe.getchildren()[0]
+            self.emitente = self.infNFe.getchildren()[1]
+            self.destinatario = self.infNFe.getchildren()[2]
 
     def nfe(self):
-        danfe = PdfTable(columns=12)
+        # danfe = PdfTable(columns=12)
         pass
-
 
     def gerar(self):
         pass
+
+if __name__ == '__main__':
+    pass
+    # import ipdb; ipdb.set_trace()
