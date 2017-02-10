13267fb42d6320203ffe8fe5e6e1c1dca774918f
Danimar Ribeiro <danimaribeiro@gmail.com>
2017-02-10 16:16:42 -0200

    WIP - Notas simpliss e ginfes layout

---
+++ pytrustnfe/test/test_ginfes.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# © 2016 Danimar Ribeiro, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+import os.path
+import unittest
+from pytrustnfe.certificado import Certificado
+from pytrustnfe.nfse.ginfes import consultar_situacao_lote
+from pytrustnfe.nfse.ginfes import consultar_nfse
+
+
+class test_nfse_ginfes(unittest.TestCase):
+
+    caminho = os.path.dirname(__file__)
+
+    def test_consulta_situacao_lote(self):
+        pfx_source = open('/home/danimar/Downloads/machado.pfx', 'r').read()
+        pfx = Certificado(pfx_source, '123456789')
+
+        dados = {'ambiente': 'homologacao'}
+        retorno = consultar_situacao_lote(
+            pfx, consulta=dados, ambiente='homologacao')
+
+        print retorno
+        self.assertNotEqual(retorno['received_xml'], '')
+        self.assertEqual(retorno['object'].Cabecalho.Sucesso, True)
