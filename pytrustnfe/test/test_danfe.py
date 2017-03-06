c28b361b31628f3671c9fba51b4f3dd64e3ff52f
Danimar Ribeiro <danimaribeiro@gmail.com>
2017-03-06 11:19:26 -0300

    Código e descrição do produto com quebra de linha Últimos ajustes na layout do danfe

--- pytrustnfe/test/test_danfe.py
+++ pytrustnfe/test/test_danfe.py
@@ -14,11 +14,13 @@ class test_danfe(unittest.TestCase):
     def test_can_generate_danfe(self):
         path = os.path.join(os.path.dirname(__file__), 'XMLs')
         xml_string = open(os.path.join(path, 'NFe00000857.xml'), "r").read()
+        # xml_string = open('/home/danimar/Downloads/NFe (5).xml', "r").read()
+
         xml_element = etree.fromstring(xml_string)
 
         oDanfe = danfe(list_xml=[xml_element])
 
         # Para testar localmente o Danfe
-        with open('/home/danimar/danfe.pdf', 'w') as oFile:
-        # with tempfile.TemporaryFile(mode='w') as oFile:
+        # with open('/home/danimar/danfe.pdf', 'w') as oFile:
+        with tempfile.TemporaryFile(mode='w') as oFile:
             oDanfe.writeto_pdf(oFile)
