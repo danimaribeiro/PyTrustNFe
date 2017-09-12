fabe0e05614e6597ec4bfe910e3e608e282b15ec
Danimar Ribeiro <danimaribeiro@gmail.com>
2017-09-11 23:56:54 -0300

    Preparando pypi package para versão python >= 3.4

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
 # coding=utf-8
 from setuptools import setup, find_packages
 
-VERSION = "0.1.39"
+VERSION = "0.1.0"
 
 setup(
-    name="PyTrustNFe",
+    name="PyTrustNFe3",
     version=VERSION,
     author="Danimar Ribeiro",
     author_email='danimaribeiro@gmail.com',
     keywords=['nfe', 'mdf-e'],
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Environment :: Plugins',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU Lesser General Public License v2 or \
 later (LGPLv2+)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     packages=find_packages(exclude=['*test*']),
