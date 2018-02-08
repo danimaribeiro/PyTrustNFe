d1749031b7c25f32eb03921f335853eea8fda323
Gabriel Balog <gabriel.balog@bradootech.com>
2018-02-08 11:30:02 -0200

    [FIX] Correção na volta da prefeitura

--- pytrustnfe/xml/__init__.py
+++ pytrustnfe/xml/__init__.py
@@ -58,8 +58,6 @@ def render_xml(path, template_name, remove_empty, **nfe):
 
 
 def sanitize_response(response):
-    if 'encoding="utf-8"' in response or 'encoding="UTF-8"' in response:
-        response = re.sub('encoding="UTF-8"', '', response)
     tree = etree.fromstring(response)
     # Remove namespaces inuteis na resposta
     for elem in tree.getiterator():
