9376a3124e4e6ed95e2ac3ab9033bd9f78272eb9
Fábio Luna <fabiocluna@hotmail.com>
2018-02-26 09:42:10 -0300

    Implementa os servidores de produção de homologação da NFC-e para SVRS.

--- pytrustnfe/Servidores.py
+++ pytrustnfe/Servidores.py
@@ -97,8 +97,8 @@ def localizar_url(servico, estado, mod='55', ambiente=2):
 
 def localizar_qrcode(estado, ambiente=2):
     sigla = SIGLA_ESTADO[estado]
-    dominio = ESTADO_WS[sigla]['65'][ambiente]['servidor']
-    complemento = ESTADO_WS[sigla]['65'][ambiente][WS_NFCE_QR_CODE]
+    dominio = ESTADO_WS[sigla][NFCE_MODELO][ambiente]['servidor']
+    complemento = ESTADO_WS[sigla][NFCE_MODELO][ambiente][WS_NFCE_QR_CODE]
     if 'https://' in complemento:
         return complemento
     return "https://%s/%s" % (dominio, complemento)
@@ -137,27 +137,50 @@ METODO_WS = {
 }
 
 SVRS = {
-    NFE_AMBIENTE_PRODUCAO: {
-        'servidor': 'nfe.svrs.rs.gov.br',
-        WS_NFE_RECEPCAO_EVENTO: 'ws/recepcaoevento/recepcaoevento.asmx',
-        WS_NFE_CANCELAMENTO: 'ws/recepcaoevento/recepcaoevento.asmx',
-        WS_NFE_AUTORIZACAO: 'ws/NfeAutorizacao/NfeAutorizacao.asmx',
-        WS_NFE_RET_AUTORIZACAO: 'ws/NfeRetAutorizacao/NfeRetAutorizacao.asmx',
-        WS_NFE_CADASTRO: 'ws/CadConsultaCadastro/CadConsultaCadastro2.asmx',
-        WS_NFE_INUTILIZACAO: 'ws/nfeinutilizacao/nfeinutilizacao2.asmx',
-        WS_NFE_CONSULTA: 'ws/NfeConsulta/NfeConsulta2.asmx',
-        WS_NFE_SITUACAO: 'ws/NfeStatusServico/NfeStatusServico2.asmx',
+    NFE_MODELO: {
+        NFE_AMBIENTE_PRODUCAO: {
+            'servidor': 'nfe.svrs.rs.gov.br',
+            WS_NFE_RECEPCAO_EVENTO: 'ws/recepcaoevento/recepcaoevento.asmx',
+            WS_NFE_CANCELAMENTO: 'ws/recepcaoevento/recepcaoevento.asmx',
+            WS_NFE_AUTORIZACAO: 'ws/NfeAutorizacao/NfeAutorizacao.asmx',
+            WS_NFE_RET_AUTORIZACAO: 'ws/NfeRetAutorizacao/NfeRetAutorizacao.asmx',
+            WS_NFE_CADASTRO: 'ws/CadConsultaCadastro/CadConsultaCadastro2.asmx',
+            WS_NFE_INUTILIZACAO: 'ws/nfeinutilizacao/nfeinutilizacao2.asmx',
+            WS_NFE_CONSULTA: 'ws/NfeConsulta/NfeConsulta2.asmx',
+            WS_NFE_SITUACAO: 'ws/NfeStatusServico/NfeStatusServico2.asmx',
+        },
+        NFE_AMBIENTE_HOMOLOGACAO: {
+            'servidor': 'nfe-homologacao.svrs.rs.gov.br',
+            WS_NFE_RECEPCAO_EVENTO: 'ws/recepcaoevento/recepcaoevento.asmx',
+            WS_NFE_CANCELAMENTO: 'ws/recepcaoevento/recepcaoevento.asmx',
+            WS_NFE_AUTORIZACAO: 'ws/NfeAutorizacao/NfeAutorizacao.asmx',
+            WS_NFE_RET_AUTORIZACAO: 'ws/NfeRetAutorizacao/NfeRetAutorizacao.asmx',
+            WS_NFE_CADASTRO: 'ws/CadConsultaCadastro/CadConsultaCadastro2.asmx',
+            WS_NFE_INUTILIZACAO: 'ws/nfeinutilizacao/nfeinutilizacao2.asmx',
+            WS_NFE_CONSULTA: 'ws/NfeConsulta/NfeConsulta2.asmx',
+            WS_NFE_SITUACAO: 'ws/NfeStatusServico/NfeStatusServico2.asmx',
+        }
     },
-    NFE_AMBIENTE_HOMOLOGACAO: {
-        'servidor': 'nfe-homologacao.svrs.rs.gov.br',
-        WS_NFE_RECEPCAO_EVENTO: 'ws/recepcaoevento/recepcaoevento.asmx',
-        WS_NFE_CANCELAMENTO: 'ws/recepcaoevento/recepcaoevento.asmx',
-        WS_NFE_AUTORIZACAO: 'ws/NfeAutorizacao/NfeAutorizacao.asmx',
-        WS_NFE_RET_AUTORIZACAO: 'ws/NfeRetAutorizacao/NfeRetAutorizacao.asmx',
-        WS_NFE_CADASTRO: 'ws/CadConsultaCadastro/CadConsultaCadastro2.asmx',
-        WS_NFE_INUTILIZACAO: 'ws/nfeinutilizacao/nfeinutilizacao2.asmx',
-        WS_NFE_CONSULTA: 'ws/NfeConsulta/NfeConsulta2.asmx',
-        WS_NFE_SITUACAO: 'ws/NfeStatusServico/NfeStatusServico2.asmx',
+    NFCE_MODELO: {
+        NFCE_AMBIENTE_PRODUCAO: {
+            'servidor': 'nfce.svrs.rs.gov.br',
+            WS_NFCE_INUTILIZACAO: 'ws/nfeinutilizacao/nfeinutilizacao2.asmx',
+            WS_NFCE_CONSULTA: 'ws/NfeConsulta/NfeConsulta2.asmx',
+            WS_NFCE_SITUACAO: 'ws/NfeStatusServico/NfeStatusServico2.asmx',
+            WS_NFCE_AUTORIZACAO: 'ws/NfeAutorizacao/NFeAutorizacao.asmx',
+            WS_NFCE_RET_AUTORIZACAO: 'ws/NfeRetAutorizacao/NFeRetAutorizacao.asmx',
+            WS_NFCE_RECEPCAO_EVENTO: 'ws/recepcaoevento/recepcaoevento.asmx',
+        },
+        NFCE_AMBIENTE_HOMOLOGACAO: {
+            'servidor': 'nfce-homologacao.svrs.rs.gov.br',
+            WS_NFCE_INUTILIZACAO: 'ws/nfeinutilizacao/nfeinutilizacao2.asmx',
+            WS_NFCE_CONSULTA: 'ws/NfeConsulta/NfeConsulta2.asmx',
+            WS_NFCE_SITUACAO: 'ws/NfeStatusServico/NfeStatusServico2.asmx',
+            WS_NFCE_AUTORIZACAO: 'ws/NfeAutorizacao/NFeAutorizacao.asmx',
+            WS_NFCE_RET_AUTORIZACAO: 'ws/NfeRetAutorizacao/NFeRetAutorizacao.asmx',
+            WS_NFCE_RECEPCAO_EVENTO: 'ws/recepcaoevento/recepcaoevento.asmx',
+            WS_NFCE_QR_CODE: 'http://dec.fazenda.df.gov.br/ConsultarNFCe.aspx',
+        }
     }
 }
 
