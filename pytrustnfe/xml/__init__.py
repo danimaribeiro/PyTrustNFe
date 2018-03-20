56c1deaebf133876e9cfcbda18723a33287a7919
Felipe <paloschi.eca@gmail.com>
2018-03-20 17:29:44 -0300

    bug encode nfse paulistana

--- pytrustnfe/xml/__init__.py
+++ pytrustnfe/xml/__init__.py
@@ -46,7 +46,7 @@ def render_xml(path, template_name, remove_empty, **nfe):
 
 
 def sanitize_response(response):
-    tree = etree.fromstring(response.encode('utf-8'))
+    tree = etree.fromstring(response)
     # Remove namespaces inuteis na resposta
     for elem in tree.getiterator():
         if not hasattr(elem.tag, 'find'):
