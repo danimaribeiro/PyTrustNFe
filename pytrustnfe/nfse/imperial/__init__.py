d8f20e32b76a277998981fa9b7f7aab7d2d47382
Felipe <paloschi.eca@gmail.com>
2018-03-21 15:34:33 -0300

    correcao nfse imperial

--- pytrustnfe/nfse/imperial/__init__.py
+++ pytrustnfe/nfse/imperial/__init__.py
@@ -20,11 +20,9 @@ def _send(certificado, method, **kwargs):
         base_url = 'https://nfe.etransparencia.com.br/rj.petropolis/webservice/aws_nfe.aspx'  # noqa
     else:
         base_url = 'https://nfehomologacao.etransparencia.com.br/rj.petropolis/webservice/aws_nfe.aspx'  # noqa
-
     xml_send = kwargs["xml"]
     path = os.path.join(os.path.dirname(__file__), 'templates')
-    soap = render_xml(path, 'SoapRequest.xml', False, soap_body=xml_send)
-
+    soap = render_xml(path, 'SoapRequest.xml', False, soap_body=xml_send.decode())
     client = HttpClient(base_url)
     response = client.post_soap(soap, 'NFeaction/AWS_NFE.%s' % method)
     response, obj = sanitize_response(response.encode('utf-8'))
