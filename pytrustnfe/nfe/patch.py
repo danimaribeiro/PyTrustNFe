9bd7b396d019cd8c359165dace458837adc89fe3
marinaGD <mgd.marinadomingues@gmail.com>
2018-12-19 16:50:49 -0200

    fix flake8

--- pytrustnfe/nfe/patch.py
+++ pytrustnfe/nfe/patch.py
@@ -1,10 +1,14 @@
 
 
 def nfeInutilizacaoCE(session, xml_send):
-    soap = '<Envelope xmlns="http://www.w3.org/2003/05/soap-envelope"><Body><nfeDadosMsg xmlns="http://www.portalfiscal.inf.br/nfe/wsdl/NFeInutilizacao4">' + xml_send + '</nfeDadosMsg></Body></Envelope>'
+    soap = '<Envelope xmlns="http://www.w3.org/2003/05/soap-envelope"><Body>\
+<nfeDadosMsg xmlns="http://www.portalfiscal.inf.br/nfe/wsdl/NFeInutilizacao4"\
+>' + xml_send + '</nfeDadosMsg></Body></Envelope>'
     headers = {
         'SOAPAction': "",
         'Content-Type': 'application/soap+xml; charset="utf-8"'
     }
-    response = session.post('https://nfeh.sefaz.ce.gov.br/nfe4/services/NFeInutilizacao4', data=soap, headers=headers)
+    response = session.post(
+        'https://nfeh.sefaz.ce.gov.br/nfe4/services/NFeInutilizacao4',
+        data=soap, headers=headers)
     return response
