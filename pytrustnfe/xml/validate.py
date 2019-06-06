f747c1e0e55625635853a9107cdf57bd722f5890
maiconkkl <contatomichael@outlook.com.br>
2019-06-06 12:09:18 -0300

    Alterando função da validação de esquema para não ser feito repetições e poder reutilizar a função só mudando o xsd; Foi criado a função que valida o esquema do webservice de distribuição do DFe, que não havia validações; Foi alterado o template da função de consultra distribuição do DFe, permitindo consulta via NSU.

--- pytrustnfe/xml/validate.py
+++ pytrustnfe/xml/validate.py
@@ -7,12 +7,22 @@ import os
 from lxml import etree
 
 PATH = os.path.dirname(os.path.abspath(__file__))
-SCHEMA = os.path.join(PATH, 'schemas/enviNFe_v4.00.xsd')
 
 
 def valida_nfe(xml_nfe):
-    nfe = etree.fromstring(xml_nfe)
-    esquema = etree.XMLSchema(etree.parse(SCHEMA))
-    esquema.validate(nfe)
+    xsd = 'schemas/enviNFe_v4.00.xsd'
+    return valida_esquema(xml_nfe, xsd)
+
+
+def valida_distribuicao(xml):
+    xsd = 'distDFeInt_v1.01.xsd'
+    return valida_esquema(xml, xsd)
+
+
+def valida_esquema(xml, xsd_name):
+    xsd = os.path.join(PATH, xsd_name)
+    xml_etree = etree.fromstring(xml)
+    esquema = etree.XMLSchema(etree.parse(xsd))
+    esquema.validate(xml_etree)
     erros = [x.message for x in esquema.error_log]
     return "\n".join(erros)
