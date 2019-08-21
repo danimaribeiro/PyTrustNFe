f23ce3eefe07f7c4a81c49822b71327fc066e59a
Danimar Ribeiro <danimaribeiro@gmail.com>
2019-08-21 17:00:44 -0300

    [WIP] Implementação de NFSe Aparecida SP.

---
+++ pytrustnfe/nfse/aparecida/__init__.py
@@ -0,0 +1,81 @@
+# © 2019 Danimar Ribeiro, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+import os
+from requests import Session
+from zeep import Client
+from zeep.transports import Transport
+from requests.packages.urllib3 import disable_warnings
+
+from pytrustnfe.xml import render_xml, sanitize_response
+from pytrustnfe.certificado import extract_cert_and_key_from_pfx, save_cert_key
+from pytrustnfe.nfe.assinatura import Assinatura
+
+
+def _render(certificado, method, **kwargs):
+    path = os.path.join(os.path.dirname(__file__), 'templates')
+    xml_send = render_xml(path, '%s.xml' % method, True, **kwargs)
+
+    reference = ''
+    signer = Assinatura(certificado.pfx, certificado.password)
+    xml_send = signer.assina_xml(xml_send, reference)
+    return xml_send
+
+
+def _send(certificado, method, **kwargs):
+    base_url = ''
+    if kwargs['ambiente'] == 'producao':
+        base_url = 'https://aparecida.siltecnologia.com.br/tbw/services/Abrasf10?wsdl'
+    else:
+        base_url = 'https://aparecida.siltecnologia.com.br/tbwhomologacao/services/Abrasf10?wsdl'
+
+    cert, key = extract_cert_and_key_from_pfx(
+        certificado.pfx, certificado.password)
+    cert, key = save_cert_key(cert, key)
+
+    disable_warnings()
+    session = Session()
+    session.cert = (cert, key)
+    session.verify = False
+    transport = Transport(session=session)
+
+    client = Client(base_url, transport=transport)
+
+    xml_send = kwargs['xml']
+    response = client.service[method](xml_send)
+    response, obj = sanitize_response(response)
+    return {
+        'sent_xml': xml_send,
+        'received_xml': response,
+        'object': obj
+    }
+
+
+def xml_recepcionar_lote_rps(certificado, **kwargs):
+    return _render(certificado, 'recepcionarLoteRps', **kwargs)
+
+
+def recepcionar_lote_rps(certificado, **kwargs):
+    if "xml" not in kwargs:
+        kwargs['xml'] = xml_recepcionar_lote_rps(certificado, **kwargs)
+    return _send(certificado, 'recepcionarLoteRps', **kwargs)
+
+
+def xml_consultar_lote_rps(certificado, **kwargs):
+    return _render(certificado, 'consultarLoteRps', **kwargs)
+
+
+def consultar_lote_rps(certificado, **kwargs):
+    if "xml" not in kwargs:
+        kwargs['xml'] = xml_consultar_lote_rps(certificado, **kwargs)
+    return _send(certificado, 'consultarLoteRps', **kwargs)
+
+
+def xml_cancelar_nfse(certificado, **kwargs):
+    return _render(certificado, 'cancelarNfse', **kwargs)
+
+
+def cancelar_nfse(certificado, **kwargs):
+    if "xml" not in kwargs:
+        kwargs['xml'] = xml_cancelar_nfse(certificado, **kwargs)
+    return _send(certificado, 'cancelarNfse', **kwargs)
