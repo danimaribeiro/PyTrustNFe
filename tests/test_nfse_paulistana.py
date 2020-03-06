de831ee38e560bfff3ea9b8cd7b28b56cc702815
regianenas <47865051+regianenas@users.noreply.github.com>
2020-03-06 20:25:27 -0300

    RN Fix template for carga tributaria (#10)
    * feat(nfse) allowing carga tibutar appear or not on the NF.
    
    * feat(nfse) fixing tests.
    
    * feat(nfse) tests.

--- tests/test_nfse_paulistana.py
+++ tests/test_nfse_paulistana.py
@@ -3,16 +3,23 @@
 import mock
 import os.path
 import unittest
+
+import pytest
+from lxml import etree
+
+from pytrustnfe import xml
 from pytrustnfe.certificado import Certificado
+from pytrustnfe.nfse import paulistana
 from pytrustnfe.nfse.paulistana import envio_lote_rps
 from pytrustnfe.nfse.paulistana import cancelamento_nfe
+from pytrustnfe.xml import sanitize_response
 
 
 class test_nfse_paulistana(unittest.TestCase):
 
     caminho = os.path.dirname(__file__)
-
-    def _get_nfse(self):
+  
+    def _get_rps(self):
         rps = [
             {
                 "assinatura": "123",
@@ -41,6 +48,44 @@ class test_nfse_paulistana(unittest.TestCase):
                 "descricao": "Venda de servico",
             }
         ]
+        return rps
+
+    def _get_rps_carga_tributaria(self):
+        rps = [
+            {
+                "assinatura": "123",
+                "serie": "1",
+                "numero": "1",
+                "data_emissao": "2016-08-29",
+                "codigo_atividade": "07498",
+                "total_servicos": "2.00",
+                "total_deducoes": "3.00",
+                "prestador": {"inscricao_municipal": "123456"},
+                "tomador": {
+                    "tipo_cpfcnpj": "1",
+                    "cpf_cnpj": "12345678923256",
+                    "inscricao_municipal": "123456",
+                    "razao_social": "Trustcode",
+                    "tipo_logradouro": "1",
+                    "logradouro": "Vinicius de Moraes, 42",
+                    "numero": "42",
+                    "bairro": "Corrego",
+                    "cidade": "Floripa",
+                    "uf": "SC",
+                    "cep": "88037240",
+                },
+                "codigo_atividade": "07498",
+                "aliquota_atividade": "5.00",
+                "descricao": "Venda de servico",
+                "valor_carga_tributaria": "30.00",
+                "fonte_carga_tributaria": "IBPT"
+
+            }
+        ]
+        return rps
+
+    def _get_nfse(self, rps):
+
         nfse = {
             "cpf_cnpj": "12345678901234",
             "data_inicio": "2016-08-29",
@@ -53,7 +98,8 @@ class test_nfse_paulistana(unittest.TestCase):
         pfx_source = open(os.path.join(self.caminho, "teste.pfx"), "rb").read()
         pfx = Certificado(pfx_source, "123456")
 
-        nfse = self._get_nfse()
+        rps = self._get_rps()
+        nfse = self._get_nfse(rps)
         path = os.path.join(os.path.dirname(__file__), "XMLs")
         xml_return = open(os.path.join(path, "paulistana_resultado.xml"), "r").read()
 
@@ -75,7 +121,8 @@ class test_nfse_paulistana(unittest.TestCase):
         pfx_source = open(os.path.join(self.caminho, "teste.pfx"), "rb").read()
         pfx = Certificado(pfx_source, "123456")
 
-        nfse = self._get_nfse()
+        rps = self._get_rps()
+        nfse = self._get_nfse(rps)
         path = os.path.join(os.path.dirname(__file__), "XMLs")
         xml_sent = open(os.path.join(path, "paulistana_signature.xml"), "r").read()
 
@@ -138,3 +185,36 @@ class test_nfse_paulistana(unittest.TestCase):
             self.assertEqual(retorno["received_xml"], xml_return)
             self.assertEqual(retorno["object"].Cabecalho.Sucesso, False)
             self.assertEqual(retorno["object"].Erro.ChaveNFe.NumeroNFe, 446)
+
+    def test_nfse_com_carga_tributaria(self):
+
+        rps = self._get_rps_carga_tributaria()
+        nfse = self._get_nfse(rps)
+
+        path = os.path.join(os.path.dirname(paulistana.__file__), 'templates')
+
+        xml_send = xml.render_xml(path, 'EnvioLoteRPS.xml', False, nfse=nfse)
+
+        _, xml_send_obj = sanitize_response(xml_send)
+
+        self.assertEqual(xml_send_obj.RPS.ValorCargaTributaria, 30.00)
+        self.assertEqual(xml_send_obj.RPS.FonteCargaTributaria, "IBPT")
+
+    def test_nfse_sem_carga_tributaria(self):
+
+        rps = self._get_rps()
+        nfse = self._get_nfse(rps)
+
+        path = os.path.join(os.path.dirname(paulistana.__file__), 'templates')
+
+        xml_send = xml.render_xml(path, 'EnvioLoteRPS.xml', False, nfse=nfse)
+
+        _, xml_send_obj = sanitize_response(xml_send)
+
+        with pytest.raises(AttributeError) as error:
+            xml_send_obj.RPS.ValorCargaTributaria
+            self.assertEqual(error.match, "no such child: ValorCargaTributaria")
+
+            xml_send_obj.RPS.FonteCargaTributaria
+            self.assertEqual(error.match, "no such child: FonteCargaTributaria")
+
