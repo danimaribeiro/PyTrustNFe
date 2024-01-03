9315556c8bcc8871e81432f03d9110d5b9d1619f
Danimar Ribeiro <danimaribeiro@gmail.com>
2024-01-03 19:28:30 -0300

    Trying to fix pyopenssl

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ later (LGPLv2+)",
         'xmlsec >= 1.3.13',  # apt update;apt install libxmlsec1-dev pkg-config -y
         'lxml >= 4.2.1',
         'cryptography >= 3.4.8',
-        'pyOpenSSL >= 17.5.0',
+        'pyOpenSSL >= 17.5.0 <= 23.2.0',
         'certifi >=  2018.1.18',
         'urllib3',
         'Jinja2',
